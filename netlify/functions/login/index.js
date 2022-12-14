3bfb4e1eddad7c4f667481655b09f82c0db70557
Edward Tran <edtran@microsoft.com>
2022-12-14 10:36:05 -0800

    Create JWT token

--- netlify/functions/login/index.js
+++ netlify/functions/login/index.js
@@ -1,10 +1,25 @@
+const jwt = require("jsonwebtoken");
+
 exports.handler = async function (event, context) {
     const {httpMethod} = event;
 
     if (httpMethod === 'POST') {
+        const expiresIn = 5 * 60;
+        const sub = 'sub';
+        const iss = process.env.ISSUER
+        const payload = {
+            sub,
+            iss
+        };
+
+        const token = jwt.sign(payload, process.env.PRIVATE_KEY, {
+            algorithm: "HS256",
+            expiresIn
+        });
+
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: "login" })
+            body: token
         }
     }
 
