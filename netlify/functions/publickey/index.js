e949be837b8e3f03f5a17155ddea960e4a3ee4ea
Edward Tran <edtran@microsoft.com>
2022-12-14 11:56:45 -0800

    Add CORS headers

--- netlify/functions/publickey/index.js
+++ netlify/functions/publickey/index.js
@@ -1,5 +1,12 @@
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+};
+
 exports.handler = async function (event, context) {
     return {
+        headers,
         statusCode: 200,
         body: process.env.PUBLIC_KEY
     }
